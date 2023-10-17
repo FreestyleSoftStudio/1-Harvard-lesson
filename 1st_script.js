1b6c4ce79af7e13b23bfa8ab3bc20b1d9b2c8616
FreestyleSoftStudio <148187561+FreestyleSoftStudio@users.noreply.github.com>
2023-10-17 18:40:05 +0300

    Initial commit

---
+++ 1st_script.js
@@ -0,0 +1,17 @@
+// Просимо клієнта підтвердити бронювання готелю
+// і зберігаємо результат роботи confirm у змінну
+const isComing = confirm("Do you love Freestyle?");
+console.log(isComing);
+
+// Запитуємо назву готеля, в якому хотів би зупинитися клієнт
+// і зберігаємо результат виклику prompt у змінну.
+const hotelName = prompt("Please enter your name in Games of Sultan");
+console.log(hotelName);
+
+const t = confirm("Hello, dear " + hotelName);
+console.log(t);
+
+const value = prompt("Please enter a number!");
+console.log(typeof value); // "string"
+console.log(value); // "5"
+
